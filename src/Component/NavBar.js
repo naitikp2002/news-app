8505ac0c79b47af50b13380408c0912a8016ae9e
Naitik Patel <naitik2002patel@gmail.com>
2022-06-06 18:10:04 +0530

    CORS error

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -4,20 +4,6 @@ import { Container } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
 function NavBar() {
   return (
-  //   <div><>
-  //   <Navbar bg="light" variant="light">
-  //     <Container>
-  //     <Navbar.Brand href="/business">Navbar</Navbar.Brand>
-  //     <Nav className="me-auto">
-  //       <Nav.Link href="/business">Business</Nav.Link>
-  //       <Nav.Link href="/entertainment">Entertainment</Nav.Link>
-  //       <Nav.Link href="/health">Health</Nav.Link>
-  //       <Nav.Link href="/science">Science</Nav.Link>
-  //       <Nav.Link href="/sports">Sports</Nav.Link>
-  //     </Nav>
-  //     </Container>
-  //   </Navbar>
-  // </></div>
   <Navbar bg="light" expand="lg">
   <Container fluid>
     <Navbar.Brand href="/">News Adda</Navbar.Brand>
